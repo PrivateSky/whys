865f324ae65f45a45ba8199bf3e831cd0910b51a
CiprianTalmacel <cprn_talmacel@yahoo.com>
2016-03-08 14:02:29 +0200

    Greatly enhanced error catching and logging in whys

--- test/test1.js
+++ test/test1.js
@@ -4,22 +4,29 @@
 
 
 require("../lib/why.js");
+process.on("error",function(){})
 
 function f1() {
-
-    try {
+    try{
         f2.why("f1 to f2")();
     }
-    catch(err){}
+    catch(err){
+        f5.why("Error treatment")();
+    }
+    setTimeout.why("Frist timeout")(f4.why("from f2").why("from f22"),10);
 }
 
 function f2(){
     setTimeout.why("Frist timeout")(f4.why("from f2").why("from f22"),10);
-    //throw new Error("Some error")
+    throw new Error("Some error");
 }
-
-
 function f4(){
+    f5();
 }
+function f5(){
+    f6.why("f5 to f6")();
+}
+function f6(){};
+
 
 f1.why("FirstCallOfF1")("FirstCallOfF1");
